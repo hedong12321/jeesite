4b3363c0691e13b16078e0e93a9a4b2c68bbd86c
donghe <comeonhd@sina.com>
2018-01-20 19:13:13 +0800

    缓存切换到redis

--- src/main/java/com/thinkgem/jeesite/common/utils/JedisUtils.java
+++ src/main/java/com/thinkgem/jeesite/common/utils/JedisUtils.java
@@ -593,7 +593,7 @@ public class JedisUtils {
 	/**
 	 * 移除Map缓存中的值
 	 * @param key 键
-	 * @param value 值
+	 * @param mapKey 值
 	 * @return
 	 */
 	public static long mapRemove(String key, String mapKey) {
@@ -614,7 +614,7 @@ public class JedisUtils {
 	/**
 	 * 移除Map缓存中的值
 	 * @param key 键
-	 * @param value 值
+	 * @param mapKey 值
 	 * @return
 	 */
 	public static long mapObjectRemove(String key, String mapKey) {
@@ -635,7 +635,7 @@ public class JedisUtils {
 	/**
 	 * 判断Map缓存中的Key是否存在
 	 * @param key 键
-	 * @param value 值
+	 * @param mapKey 值
 	 * @return
 	 */
 	public static boolean mapExists(String key, String mapKey) {
@@ -656,7 +656,7 @@ public class JedisUtils {
 	/**
 	 * 判断Map缓存中的Key是否存在
 	 * @param key 键
-	 * @param value 值
+	 * @param mapKey 值
 	 * @return
 	 */
 	public static boolean mapObjectExists(String key, String mapKey) {
@@ -783,28 +783,30 @@ public class JedisUtils {
 	/**
 	 * 归还资源
 	 * @param jedis
-	 * @param isBroken
+	 * @param jedis
 	 */
 	public static void returnBrokenResource(Jedis jedis) {
 		if (jedis != null) {
-			jedisPool.returnBrokenResource(jedis);
+		    jedis.close();
+			//jedisPool.returnBrokenResource(jedis);
 		}
 	}
 	
 	/**
 	 * 释放资源
 	 * @param jedis
-	 * @param isBroken
+	 * @param jedis
 	 */
 	public static void returnResource(Jedis jedis) {
 		if (jedis != null) {
-			jedisPool.returnResource(jedis);
+			jedis.close();
+			//jedisPool.returnResource(jedis);
 		}
 	}
 
 	/**
 	 * 获取byte[]类型Key
-	 * @param key
+	 * @param object
 	 * @return
 	 */
 	public static byte[] getBytesKey(Object object){
@@ -835,7 +837,7 @@ public class JedisUtils {
 	
 	/**
 	 * Object转换byte[]类型
-	 * @param key
+	 * @param object
 	 * @return
 	 */
 	public static byte[] toBytes(Object object){
@@ -844,7 +846,7 @@ public class JedisUtils {
 
 	/**
 	 * byte[]型转换Object
-	 * @param key
+	 * @param bytes
 	 * @return
 	 */
 	public static Object toObject(byte[] bytes){
